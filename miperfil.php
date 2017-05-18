d1f11d7bb81ab317b058a6f6b76e32e1d6acc6e4
1daw <1daw>
2017-05-18 12:32:58 +0200

    subida miperfil y imagenes

---
+++ miperfil.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MiPerfil</title>
+    <link rel="stylesheet" href="miperfil.css">
+
+  </head>
+  <body>
+    Nombre de Usuario: <br>
+    <input type="text" name="usuario" value>
+    <br>
+
+    <div class="img">
+      <img src="logobueno.jpg" alt="logo">
+    </div>
+
+    <div class="textos">
+    <div class="textoA">
+      <h4>Sobre mi:</h4>
+      <textarea name="name" rows="15" cols="40" wrap="hard" readonly="yes">
+      </textarea>
+    </div>
+
+
+    <div class="textoB">
+      <h4>Mis gustos:</h4>
+      <textarea name="name" rows="15" cols="40" wrap="hard" readonly="yes"></textarea>
+    </div>
+    </div>
+
+    <br>
+    <br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="buscarpersonas">
+
+      <p>Buscar personas</p>
+
+    </div>
+
+  </body>
+
+  <footer id="footer1">
+    <img src="banner.jpg" alt="100">
+
+
+  </footer>
+
+
+  <footer id="footer2">
+    <img class="iconfacebook" src="fboo.jpg" alt="">
+    <img class="icontwitter" src="socialtwitter.png" alt="">
+    <img class="icongoogle" src="google.jpg" alt="">
+  </footer>
+
+
+</html>
