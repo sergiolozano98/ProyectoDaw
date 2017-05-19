df18a1e0728a92235f226636eab42e07ec49a86c
WINMAC3\1daw <sergiolozanobueno@gmail.com>
2017-05-19 08:52:36 +0200

    BD buena

--- datame.sql
+++ datame.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.1.21-MariaDB - mariadb.org binary distribution
+-- Versión del servidor:         10.1.13-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win32
 -- HeidiSQL Versión:             9.4.0.5125
 -- --------------------------------------------------------
@@ -25,51 +25,69 @@ CREATE TABLE IF NOT EXISTS `preguntas` (
 
 -- Volcando datos para la tabla datame.preguntas: ~6 rows (aproximadamente)
 /*!40000 ALTER TABLE `preguntas` DISABLE KEYS */;
-INSERT INTO `preguntas` (`idPregunta`, `pregunta`) VALUES
-	(1, '¿Que color de pelo tienes?'),
+REPLACE INTO `preguntas` (`idPregunta`, `pregunta`) VALUES
+	(1, '¿Que edad buscas?'),
 	(2, '¿Que buscas?'),
 	(3, '¿Orientación sexual?'),
-	(4, '¿Comida favorita?'),
-	(5, '¿Que estilo te gusta?'),
-	(6, '¿Que edad buscas?');
+	(4, '¿Que color de pelo prefieres?'),
+	(5, '¿Que alimentacion sigues?'),
+	(6, '¿Fumas?');
 /*!40000 ALTER TABLE `preguntas` ENABLE KEYS */;
 
 -- Volcando estructura para tabla datame.total
 CREATE TABLE IF NOT EXISTS `total` (
   `idUsuario` int(11) NOT NULL,
   `idPregunta` int(11) NOT NULL,
-  `idRespuesta` int(11) NOT NULL,
+  `Respuesta` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`idUsuario`,`idPregunta`),
   KEY `FK__preguntas` (`idPregunta`),
-  CONSTRAINT `FK__preguntas` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`idPregunta`),
-  CONSTRAINT `FK__usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`)
+  CONSTRAINT `FK_total_preguntas` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`idPregunta`),
+  CONSTRAINT `FK_total_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Volcando datos para la tabla datame.total: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla datame.total: ~18 rows (aproximadamente)
 /*!40000 ALTER TABLE `total` DISABLE KEYS */;
-INSERT INTO `total` (`idUsuario`, `idPregunta`, `idRespuesta`) VALUES
-	(1, 1, 3),
-	(1, 2, 2),
-	(1, 4, 4);
+REPLACE INTO `total` (`idUsuario`, `idPregunta`, `Respuesta`) VALUES
+	(2, 1, 'Entre 18 y 25'),
+	(2, 2, 'Relacion seria'),
+	(2, 3, 'Homosexual'),
+	(2, 4, 'CastaÃ±o'),
+	(2, 5, 'Vegano'),
+	(2, 6, 'si'),
+	(3, 1, 'Entre 18 y 25'),
+	(3, 2, 'Relacion seria'),
+	(3, 3, 'Bisexua'),
+	(3, 4, 'Rubio'),
+	(3, 5, 'Vegano'),
+	(3, 6, 'Otros'),
+	(4, 1, 'Mayores de 45'),
+	(4, 2, 'Relacion Relacion esporÃ¡dica'),
+	(4, 3, 'Bisexua'),
+	(4, 4, 'Otros'),
+	(4, 5, 'Vegano'),
+	(4, 6, 'Otros');
 /*!40000 ALTER TABLE `total` ENABLE KEYS */;
 
--- Volcando estructura para tabla datame.usuario
-CREATE TABLE IF NOT EXISTS `usuario` (
+-- Volcando estructura para tabla datame.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(50) COLLATE utf8_spanish_ci DEFAULT '0',
-  `nombre` varchar(50) COLLATE utf8_spanish_ci DEFAULT '0',
-  `apellidos` varchar(50) COLLATE utf8_spanish_ci DEFAULT '0',
-  `fecha_nac` date DEFAULT NULL,
-  `email` varchar(50) COLLATE utf8_spanish_ci DEFAULT '0',
-  `pass` varchar(50) COLLATE utf8_spanish_ci DEFAULT '0',
+  `usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
+  `nombre` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `apellidos` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `edad` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
+  `pass` varchar(50) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Volcando datos para la tabla datame.usuario: ~1 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`id`, `usuario`, `nombre`, `apellidos`, `fecha_nac`, `email`, `pass`) VALUES
-	(1, 'dsad', 'sda', 's', '2017-05-16', 'asd', 'asda');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+-- Volcando datos para la tabla datame.usuarios: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+REPLACE INTO `usuarios` (`id`, `usuario`, `nombre`, `apellidos`, `edad`, `email`, `pass`) VALUES
+	(1, 'Pepe9', 'Pepe', 'jajas', 24, 'jsjsj', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8'),
+	(2, 'Juanito97', 'aadasd', 'asdasdas', 22, 'asdasdasd', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8'),
+	(3, 'Sergio9', 'Sergio', 'Lozano', 21, 'sergio11', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8'),
+	(4, 'Fran99', 'fran', 'asjds', 22, 'sushfranciscquito', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
